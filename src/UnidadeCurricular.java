a5ae369b58a35f68556b1295668abcaecf02c243
vitorsantos <1252331@isep.ipp.pt>
2026-03-04 19:02:44 +0000

    Criação da classe Departamento e Atualização da classe Unidade Curricular

--- src/UnidadeCurricular.java
+++ src/UnidadeCurricular.java
@@ -1,2 +1,40 @@
+import java.time.LocalDate;
+
 public class UnidadeCurricular {
+    private String nome;
+    private LocalDate anoCurricular;
+    private Docente docente;
+
+    public UnidadeCurricular() {
+    }
+
+    public UnidadeCurricular(String nome, LocalDate anoCurricular, Docente docente) {
+        this.nome = nome;
+        this.anoCurricular = anoCurricular;
+        this.docente = docente;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getAnoCurricular() {
+        return anoCurricular;
+    }
+
+    public void setAnoCurricular(LocalDate anoCurricular) {
+        this.anoCurricular = anoCurricular;
+    }
+
+    public Docente getDocente() {
+        return docente;
+    }
+
+    public void setDocente(Docente docente) {
+        this.docente = docente;
+    }
 }
