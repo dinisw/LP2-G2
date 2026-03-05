21f8404b7d2582931678e7f379a356690b2ae459
1252039 <1252039@isep.ipp.pt>
2026-03-05 13:02:39 +0000

    Criação das classes Avaliacao e Docente.

---
+++ src/model/Avaliacao.java
@@ -0,0 +1,50 @@
+package model;
+
+public class Avaliacao {
+    private String momento;
+    private double nota;
+    private UnidadeCurricular unidadeCurricular;
+    private Estudante estudante;
+
+    public Avaliacao() {
+    }
+
+    public Avaliacao(String momento, double nota, UnidadeCurricular unidadeCurricular, Estudante estudante) {
+        this.momento = momento;
+        this.nota = nota;
+        this.unidadeCurricular = unidadeCurricular;
+        this.estudante = estudante;
+    }
+
+    public String getMomento() {
+        return momento;
+    }
+
+    public void setMomento(String momento) {
+        this.momento = momento;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+
+    public UnidadeCurricular getUnidadeCurricular() {
+        return unidadeCurricular;
+    }
+
+    public void setUnidadeCurricular(UnidadeCurricular unidadeCurricular) {
+        this.unidadeCurricular = unidadeCurricular;
+    }
+
+    public Estudante getEstudante() {
+        return estudante;
+    }
+
+    public void setEstudante(Estudante estudante) {
+        this.estudante = estudante;
+    }
+}
